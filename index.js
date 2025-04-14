e3ac0611ecc2d4e07433316b7fee7b85c53bc7e0
unknown <lipebraga162@gmail.com>
2025-04-14 15:18:08 -0300

    lopal

---
+++ index.js
@@ -0,0 +1,6 @@
+// Programa 1 - Exibe todos os números de 1 até o número digitado pelo usuário
+let numero = parseInt(prompt("Digite um número:"));
+
+for (let i = 1; i <= numero; i++) {
+    console.log(i);
+}
